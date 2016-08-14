952429300255286bce46e0b760999a6ed341b835
System Administrator <janko>
2016-08-15 00:22:00 +0800

    initial commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+Title: Kamikaze Writer
+Description: Writing app that will delete what you write after some time. Ideal for beating procastrination.
+Author: Janko Prester
+Contact: janko.prester@gmail.com
+Current version: v0.0.1
+
+File structure:
+
+Install instructions: 
+
+
+
---
+++ src/js/scripts.js
@@ -0,0 +1,24 @@
+var textSection = document.getElementById('textContent');
+var myVar;
+var time = 5;
+
+textSection.addEventListener('keydown', runTimer);
+
+function runTimer () {
+    clearTimeout(myVar);
+    myVar = setTimeout(timerAction, timerSetup(time));
+}
+
+function timerSetup(seconds) {
+    return seconds * 1000;
+}
+
+function timerAction () {
+    deleteText();
+}
+
+function deleteText () {
+    console.log("function has been run. This is inside textArea : " + textSection.value);
+    textSection.value = "";
+}
+
